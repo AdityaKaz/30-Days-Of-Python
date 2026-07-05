e7f0c5839de91cb6dccb667bdd6a6de24a46d18e
= <=>
2026-07-05 18:15:30 +0530

    completed exercises for day 3

---
+++ 03_Day_Operators/day_3.py
@@ -0,0 +1,86 @@
+# Day 3: 30 Days of python programming
+
+# age = 23
+# height = 1.83
+# complex_number = 1 + 1j
+
+# base = float(input("Enter base: "))
+# height = float(input("Enter height: "))
+# area = 0.5 * base * height
+# print(f"The area of the triangle is {area}")
+
+# a = float(input("Enter side a: "))
+# b = float(input("Enter side b: "))
+# c = float(input("Enter side c: "))
+# perimeter = a + b + c
+# print(f"The perimeter of the triangle is {perimeter}")
+
+# length = float(input("Enter length: "))
+# width = float(input("Enter width: "))
+# area = length * width
+# perimeter = 2 * (length + width)
+# print(f"The area and perimeter of rectangle are {area} and {perimeter} respectively")
+
+# radius = float(input("Enter radius: "))
+# area = 3.14 * radius * radius
+# circumference = 2 * 3.14 * radius
+# print(f"The area and circumference of circle are {area} and {circumference} respectively")
+
+# m1, c = 2, -2
+# print(f"y_intercept = (0, {c})")
+# x = -c / m1
+# print(f"x_intercept = ({x}, 0)")
+
+# x1, y1, x2, y2 = 2, 2, 6, 10
+# m2 = (y2 - y1) / (x2 - x1)
+# d = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+# print(f"Slope is {m2} and Euclidean distance is {d}")
+
+# if m1 == m2:
+#     print("The slopes are equal")
+# else:
+#     print("The slopes are not equal")
+
+# x = int(input("Enter the value of x: "))
+# y = x ** 2 + 6 * x + 9
+# print(f"When x = {x}, y = {y}")
+
+# print(len("python") > len("dragon"))
+
+# print("on" in "python" and "on" in "dragon")
+
+# sentence = "I hope this course is not full of jargon"
+# print("jargon" in sentence)
+
+# print("on" not in "python" and "on" not in "dragon")
+
+# print(str(float(len("python"))))
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# div = 7 // 3
+# given = 2.7
+# print(div == int(given))
+
+# print(type(10) == type("10"))
+
+# print(int(float("9.8")) == 10)
+
+# hours = int(input("Enter hours: "))
+# rph = int(input("Enter rate per hour: "))
+# print(f"Your weekly earning is {hours * rph}")
+
+# years = int(input("Enter number of years you have lived: "))
+# seconds_alive = years * 365 * 24 * 60 * 60
+# print(f"You have lived for {seconds_alive} seconds.")
+
+# a, b, c, d, e = 1, 2, 3, 4, 5
+# print(a, a ** 0, a ** 1, a ** 2, a ** 3)
+# print(b, b ** 0, b ** 1, b ** 2, b ** 3)
+# print(c, c ** 0, c ** 1, c ** 2, c ** 3)
+# print(d, d ** 0, d ** 1, d ** 2, d ** 3)
+# print(e, e ** 0, e ** 1, e ** 2, e ** 3)
